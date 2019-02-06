18a983d59dc91e6532a8211827c889a3095db9eb
finbarmaginn <finbarmaginn@gmail.com>
2019-02-06 14:00:39 +0000

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Wubba Lubba Fuck Up"/>
   </div>
 </template>
 
